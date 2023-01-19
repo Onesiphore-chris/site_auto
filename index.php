cc6975bdde3f39277fca38fba110d41936a5c813
Onesiphore-chris <chrisleconquerant01@gmail.com>
2023-01-19 14:05:48 +0100

    ok

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="block">
+        <div class="nav_bar">
+        <div class="card_1">
+            <img src="#" alt="">
+        </div>
+        <div class="car">
+            <div class="car_1"></div>
+            <div class="car_2"></div>
+            <div class="car_3"><a href="">HOME</a></div>
+        </div>
+        <div class="nav">
+            <a href="">SERVICE</a>
+            <a href="">A PROPOS</a>
+            <a href="">CONTACT</a>
+        </div>
+        </div>
+        <div class="img"></div>
+        <div class="car_4"></div>
+        <div class="car_5">
+            <form action="" method="post">
+                <h1>Newletter</h1>
+                <label for="">Email</label>
+                <input type="email" name="email" id=""><br>
+                <label for="">number</label>
+                <input type="numer" name="number" id="">
+                <input type="select">
+                
+
+
+                <input type="select">
+   <nom>images-type</nom>
+   <libellé>support actuel de vos images</libellé>
+   <option valeur="papier">photo papier</option>
+   <option valeur="dia">diapositives</option>
+   <option valeur="numeriques">images numériques</option>
+   <p>Si vous utilisez plusieurs formats, cochez celui qui est le plus fréquent.</p>
+</input>
+                
+            </form>
+        </div>
+        <div class="car_6"></div>
+    </header>
+
+</body>
+</html>
