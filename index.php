9f3937a85e6f0dcfeb2bdad30e4ad4c00af987e6
Onesiphore-chris <chrisleconquerant01@gmail.com>
2023-01-25 07:45:39 +0100

    mise a jour du footer et information

--- index.php
+++ index.php
@@ -26,28 +26,158 @@
         </div>
        
     </section>
-
-      <!--  section stat header -->
-      
-
-        <div class="img">
-            <img src="image/header.jpg" alt="">
-        </div>
-        <div class="car_4"></div>
-        <div class="car_5">
-            <form action="" method="post">
-                <h1>Newletter</h1>
-                <label for="">Email</label>
-                <input type="email" name="email" id=""><br>
-                <label for="">number</label>
-                <input type="numer" name="number" id="">
-                
-                
-                
-            </form>
-        </div>
-        <div class="car_6"></div>
     
 
+      <!--  section End header -->
+
+      <!--  section start sub_header -->
+      <div class="sub_header">
+      <div class="emblem">
+            <p>
+            Pour chaque propriétaire de voiture,<br>
+            vous avez besoin d’une assurance pour protéger votre voyage.
+            </p>
+        </div>
+        <div class="image">
+            <img src="image/header.jpg" alt="">
+        </div>
+        </div>
+      <!--  section End sub_header -->
+
+      <!-- creation du form et cadre -->
+
+        <div class="card2">
+            <div class="card_4">
+                <h4>CONTACT</h4>
+                <div class="list">
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                </div>
+            </div>
+            <div class="card_5">
+            <div class="title">NEWLETTER</div>
+                <form action="" method="post">
+                    <div class="user-details">
+                        <div class="input-box">
+                        <input type="text" name="pays" placeholder="*pays" required class="input-text">
+                    </div>
+                    <div class="input-box">
+                        <input type="text" name="number" placeholder="*number" required class ="input-number">
+                    </div>
+                    <div class="input-box">
+                        <input type="email" name="email" placeholder="*email" required class="input-mail">
+                    </div>
+                    
+                    </div>
+                    <div class="button">
+                        <input type="submit" value="Envoyer">
+                    </div>
+                </form>
+            </div>
+            <div class="card_6">
+                <p>Lorem  ipsum dolor sit amet consectetur adipisicing elit. 
+                    Voluptate natus culpa officia sit repellendus eius corporis nostrum. 
+                    Eius, a quas.</p>
+                    <div class="button-2">
+                        <div class="car1">
+                        
+        
+                        </div>
+                    </div>
+            </div>
+        </div>
+      <!-- FIn du form et cadre -->
+
+      <!-- creation du bloc information -->
+      <div class="info">
+        <div class="card-info">
+            <div class="card-info-1">
+                <div class="info-logo"></div>
+                <h6>+100</h6>
+                <span>persone assurer</span>
+            </div>
+            <div class="card-info-1">
+                <div class="info-logo"></div>
+                <h6>+100</h6>
+                <span>persone assurer</span>
+            </div>
+            <div class="card-info-1">
+                <div class="info-logo"></div>
+                <h6>+100</h6>
+                <span>persone assurer</span>
+            </div>
+        </div>
+      </div>
+
+      <!-- Fin du bloc information -->
+
+
+      <!-- Debut du footer -->
+
+    <footer class="footer_1">
+        <div class="bloc">
+        <div class="bloc_1">
+            <div class="logo">
+                <a href="#">CHRIS<span class="auto">|AUTO</span></a>
+                </div>
+                <div class="para">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Cum nam nesciunt unde odit repellendus quod labore nihil id,
+                        veritatis laudantium! Corporis doloremque optio itaque numquam alias. 
+                        Dolore, nihil blanditiis? Voluptas.
+                    </p>
+                </div>
+                <div class="reseau">
+                    
+                </div>
+        </div>
+        <div class="bloc_2">
+            <h4>Service</h4>
+            <div class="list">
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                </div>
+        </div>
+        <div class="bloc_3">
+            <h4> Heure De Service</h4>
+            <div class="list">
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                    <li>contact 1 </li>
+                </div>
+        </div>
+        </div>
+    </footer>
+
+
+
+
+      <!-- Fin du footer -->
+
+
+
+
+      
 </body>
 </html>
